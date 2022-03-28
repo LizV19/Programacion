f1ec102fcca29366de95d7375036facfdcfee617
LizV19 <100545004+LizV19@users.noreply.github.com>
2022-03-28 23:34:22 +0200

    Add files via upload

---
+++ Actividad2.py
@@ -0,0 +1,11 @@
+vTupla = (10,)
+print(type(vTupla))
+
+vEdades = [2, 7, 58, 7, 45, 26, 10, 8, 56, 57, 97, 19, 11, 53, 3, 99, 62, 78, 29, 9, 37, 42, 56, 86, 28, 86, 95, 26, 49, 67, 21, 815, 67, 10, 58, 512, 24, 92, 89, 67, 53, 10, 9, 83, 1, 44, 10, 77, 98, 73, 57]
+
+
+for n in vEdades:
+    if n == 10:
+        vEdades.remove(n)
+
+print("la lista sin las ocurrencias de 10 es:" ,vEdades)             
